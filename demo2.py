6e1dbb6fa4b46a94cdad71c3ddb21e6fcd23ff94
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-02-06 22:29:51 +0530

    myPackages1

---
+++ demo2.py
@@ -0,0 +1,15 @@
+from myPackages1 import function as f , greet as g
+
+print(f.add(10 , 2)) # 12
+
+print(f.sub(10 , 2)) # 8
+
+print(f.mul(10 , 2)) # 20
+
+print(f.div(50 , 3)) # 16.666...
+
+print(f.mod(40 , 7)) # 5
+
+print(f.pow(5 , 3)) # 125
+
+g.greet("Chirag") # Hello , Chirag!!
