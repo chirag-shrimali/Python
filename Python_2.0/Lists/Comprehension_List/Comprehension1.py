da7772364e402b0bd5f194b3336cefc9b6bb9b32
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-02-06 21:23:05 +0530

    comprehension

---
+++ Python_2.0/Lists/Comprehension_List/Comprehension1.py
@@ -0,0 +1,41 @@
+# write the python program and also write the comprehension version using if condition statements
+
+# displays the lists which have length more than 4
+
+# users = ["ram" , "shyam" , "amit" , "sumit" , "geeta" , "jaya" , "rama"]
+
+# users1 = []
+
+# for i in users :
+#     if len(i) > 4 :
+#         users1.append(i)
+
+# print(users1)
+
+# Comprehension Version ----------------
+
+# users2 = [i for i in users if len(i) > 4]
+
+# print(users2)
+
+# -------------------------------------------------------------------------------------------------------
+
+# Palindrome print 'yes' otherwise print 'no'
+
+# users = ["ram" , "shyam" , "amit" , "sumit" , "naman" , "geeta" , "jaya" , "rama" , "bob"]
+
+# users1 = []
+
+# # for i in users :
+# #     if(i == i[ : : -1]) :
+# #         users1.append(i)
+
+# # print(users1)
+
+# for i in users :
+#     if(i == i[ : : -1]) :
+#         print('Yes')
+#     else :
+#         print('No')
+
+# print()
