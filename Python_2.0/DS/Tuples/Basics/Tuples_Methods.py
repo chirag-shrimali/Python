c8628a3504c813bed2490b2dc52ca2dbb12ad5c9
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-02-09 23:32:38 +0530

    Tuples Methods

--- Python_2.0/DS/Tuples/Basics/Tuples_Methods.py
+++ Python_2.0/DS/Tuples/Basics/Tuples_Methods.py
@@ -24,16 +24,31 @@
 
 #             0          1          2          3          4          5          6          7 
 
+# users = ("Chirag" , 'Ramesh' , "Mukesh" , 'Ganesh' , "Paresh" , 'Ramesh' , "Suresh" , "Ramesh")
+
+# print(users) # prints all the elements of the users tuples
+
+# print(type(users)) # <class 'tuple'>
+
+# print(type(users).__name__) # tuple as string print
+
+# print(users.index("Mukesh")) # 2 it will be print the index value of the elements if they are present in the tuples otherwise it will be showing errors
+
+# print(users.index('Ramesh')) # 1 if the elements are reapeating multiple times the index() function will be print the first index value of that elements that where that elements are comes first times
+
+# print(users.index("chirag")) # error shows that tuple index which are not present in the existing tuples
+
+# -----------------------------------------------
+
+#           0           1          2          3          4           5         6          7
+
 users = ("Chirag" , 'Ramesh' , "Mukesh" , 'Ganesh' , "Paresh" , 'Ramesh' , "Suresh" , "Ramesh")
 
-print(users) # prints all the elements of the users tuples
+name = "Raj"
 
-print(type(users)) # <class 'tuple'>
+if name in users :
+    idx = users.index(name)
+    print(f"\n{name} is found at {idx} index...")
 
-print(type(users).__name__) # tuple as string print
-
-print(users.index("Mukesh")) # 2 it will be print the index value of the elements if they are present in the tuples otherwise it will be showing errors
-
-print(users.index('Ramesh')) # 1 if the elements are reapeating multiple times the index() function will be print the first index value of that elements that where that elements are comes first times
-
-print(users.index("chirag")) # error shows that tuple index which are not present in the existing tuples
+else :
+    print(f"\n{name} is not found in the given tuples...")
