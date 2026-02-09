079d7564813e604b9ef7de3e473dde1c14c549e7
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-02-09 23:38:13 +0530

    Type Casting

---
+++ Python_2.0/DS/Tuples/Conversions/type_casting.py
@@ -0,0 +1,19 @@
+#           0           1          2          3          4           5         6          7
+
+users = ("Chirag" , 'Ramesh' , "Mukesh" , 'Ganesh' , "Paresh" , 'Ramesh' , "Suresh" , "Ramesh")
+
+print(users)
+
+usersList = list(users)
+
+print(usersList)
+
+print("----------------------------------------------------------------------------------------------")
+
+usersList.append('Rahul')
+
+print(usersList)
+
+usersTuple = tuple(usersList)
+
+print(usersTuple)
