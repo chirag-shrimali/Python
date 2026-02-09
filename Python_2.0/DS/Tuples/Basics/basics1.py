ac3733648c68de65e53a70e0ce56350601863136
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-02-09 23:29:02 +0530

    Basic1 Tuples

---
+++ Python_2.0/DS/Tuples/Basics/basics1.py
@@ -0,0 +1,23 @@
+t1 = (1 , 2 , 3)
+
+t2 = (4 , 5 , 6)
+
+a = t1 + t2
+
+print(a) # (1 , 2 , 3 , 4 , 5 , 6) when tuple and another also tuple at the end of tuple 1 tuple 2 elements adding in squence but remember that tuple - interger and integer - tuple are not working with +
+
+#     2   +   6  = 8
+
+b = t1[1] + t2[2] # 8
+
+print(b)
+
+#      1  + (4 , 5 , 6)
+
+# c = t1[0] + t2
+
+# print(c) # error comes due to intger and tuple both are not work with + operators
+
+# (1 , 2 , 3) + 5
+
+# d = t1 + t2[1] # error comes due to the tuple and int are not supported both operands are not work with + operators
