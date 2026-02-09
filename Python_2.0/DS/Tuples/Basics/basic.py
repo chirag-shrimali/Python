863ba6e58a3f9f0302d85f05c930e88e88255c46
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-02-09 18:54:32 +0530

    Basics Tuples

---
+++ Python_2.0/DS/Tuples/Basics/basic.py
@@ -0,0 +1,41 @@
+# in the python the parts of data structures has data types like tuples
+
+# the declarations of Tuples are : ()
+
+# Empty Tuple ---------------------------------------------
+
+# t = ()
+
+# print(t) # () as Empty Tuples
+
+# print(type(t)) # <class 'tuple'>
+
+# print(type(t).__name__) # tuple as string
+
+# Single Tuple Elements ------------------------------------
+
+# t = (1 ,)
+
+# print(t) # (1 ,)
+
+# print(type(t)) # <class 'tuple'>
+
+# print(type(t).__name__) # tuple as string
+
+# t = (1) # or we can be write as t = 1
+
+# print(t) # 1
+
+# print(type(t)) # <class 'int'>
+
+# print(type(t).__name__) # int
+
+# Multiple Elements in the tuples ---------------------------------------------------
+
+t = (4 , 5 , "Chirag" , "Ahmedabad" , "Chirag@1234" , 99)
+
+print(t) # print all the elements which are present in the tuples
+
+print(type(t)) # <class 'tuple'>
+
+print(type(t).__name__) # tuple as string prints
