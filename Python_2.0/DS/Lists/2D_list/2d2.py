8b027a2f22ff533ac54ca1ddc49063fccbde88de
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-02-13 22:21:06 +0530

    2D Lists

---
+++ Python_2.0/DS/Lists/2D_list/2d2.py
@@ -0,0 +1,13 @@
+# 2D Lists...
+
+items = [    
+# 0          0   1
+            [1 , 2] , 
+# 1          
+            [3 , 4] , 
+# 2     
+            [5 , 6]
+        ]
+
+for i , j in items :
+    print(i , j)
