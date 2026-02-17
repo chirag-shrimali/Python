02779f22eebf1ff5abc4c0dd152504fbce0d1ec1
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-02-17 20:49:16 +0530

    Single Inheritance Public

---
+++ Oop's/Inheritances/Single_Inheritance/single_public.py
@@ -0,0 +1,116 @@
+"""  
+There are having 5 types of inheritances...
+
+1. Single Inheritance
+2. Multi - Level Inheritance
+3. Hierarchical Inheritance
+4. Multiple Inheritance
+5. Hybrid Inheritance
+"""
+
+# Single Inheritance :-
+
+"""
+    A
+    |
+    B(A) object to be created...
+"""
+
+# Scope Visibility (Public) ------------------------------------------------------------
+
+# class student :
+#     def __init__(self):
+#         self.name = "Chirag"
+#         self.age = 19
+
+# class teacher(student) :
+#     def __init__(self):
+#         student.__init__(self)
+#         self.t_name = "Ramesh Shah"
+
+#     def show(self) :
+#         print("\n======= Student Information =======")
+#         print("\nStudent Name is :",self.name)
+#         print("\nStudent Age is :",self.age)
+#         print("\n======= Teacher Information =======")
+#         print("\nTeacher Name is :",self.t_name)
+
+# t = teacher()
+
+# t.show()
+
+# ----------------------------------------------------------------------------------
+
+# class student :
+#     def __init__(self):
+#         self.name = "Chirag"
+#         self.age = 19
+
+# class teacher(student) :
+#     def __init__(self):
+#         super().__init__()
+#         self.t_name = "Ramesh Shah"
+
+#     def show(self) :
+#         print("\n======= Student Information =======")
+#         print("\nStudent Name is :",self.name)
+#         print("\nStudent Age is :",self.age)
+#         print("\n======= Teacher Information =======")
+#         print("\nTeacher Name is :",self.t_name)
+
+# t = teacher()
+
+# t.show()
+
+# ------------------------------------------------------------------------------------------------
+
+# class student :
+#     # def __init__(self):
+#         name = "Chirag"
+#         age = 19
+
+# class teacher(student) :
+#     def __init__(self):
+#         self.t_name = "Ramesh Shah"
+
+#     def show(self) :
+#         print("\n======= Student Information =======")
+#         print("\nStudent Name is :",self.name)
+#         print("\nStudent Age is :",self.age)
+#         print("\n======= Teacher Information =======")
+#         print("\nTeacher Name is :",self.t_name)
+
+# t = teacher()
+
+# t.show()
+
+# Update or Modify ---------------------------------------------------------------------------------------------------
+
+class student :
+    def __init__(self):
+        self.name = "Chirag"
+        self.age = 19
+
+class teacher(student) :
+    def __init__(self):
+        student.__init__(self)
+        self.t_name = "Ramesh Shah"
+
+    def show(self) :
+        print("\n======= Student Information =======")
+        print("\nStudent Name is :",self.name)
+        print("\nStudent Age is :",self.age)
+        print("\n======= Teacher Information =======")
+        print("\nTeacher Name is :",self.t_name)
+
+t = teacher()
+
+t.show()
+
+print("\n------------------------------------------------------------------")
+
+t.name = "Chintan"
+
+t.age = 45
+
+t.show()
