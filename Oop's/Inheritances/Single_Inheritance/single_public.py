7fdf8d920786e46a82b76980724b851b3efbde6a
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-02-17 21:25:09 +0530

    Single Public

--- Oop's/Inheritances/Single_Inheritance/single_public.py
+++ Oop's/Inheritances/Single_Inheritance/single_public.py
@@ -86,31 +86,56 @@ There are having 5 types of inheritances...
 
 # Update or Modify ---------------------------------------------------------------------------------------------------
 
+# class student :
+#     def __init__(self):
+#         self.name = "Chirag"
+#         self.age = 19
+
+# class teacher(student) :
+#     def __init__(self):
+#         student.__init__(self)
+#         self.t_name = "Ramesh Shah"
+
+#     def show(self) :
+#         print("\n======= Student Information =======")
+#         print("\nStudent Name is :",self.name)
+#         print("\nStudent Age is :",self.age)
+#         print("\n======= Teacher Information =======")
+#         print("\nTeacher Name is :",self.t_name)
+
+# t = teacher()
+
+# t.show()
+
+# print("\n------------------------------------------------------------------")
+
+# t.name = "Chintan"
+
+# t.age = 45
+
+# t.show()
+
+# Parameterized Constrcutors -----------------------------------------------------------------
+
 class student :
-    def __init__(self):
-        self.name = "Chirag"
-        self.age = 19
+    def __init__(self , name , age , city):
+        self.name = name
+        self.age = age
+        self.city = city
 
 class teacher(student) :
-    def __init__(self):
-        student.__init__(self)
-        self.t_name = "Ramesh Shah"
+    def __init__(self , name , age , city):
+        student.__init__(self , name , age , city)
+        self.t_name = "Kishor Kumar"
 
     def show(self) :
         print("\n======= Student Information =======")
         print("\nStudent Name is :",self.name)
         print("\nStudent Age is :",self.age)
+        print("\nStudent City is :",self.city)
         print("\n======= Teacher Information =======")
         print("\nTeacher Name is :",self.t_name)
 
-t = teacher()
-
-t.show()
-
-print("\n------------------------------------------------------------------")
-
-t.name = "Chintan"
-
-t.age = 45
+t = teacher("Chirag" , 19 , "Srinagar")
 
 t.show()
