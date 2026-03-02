61e5a554432c15dac36cb0c540085a681967b118
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-03-02 19:14:08 +0530

    Set Basics

---
+++ Python_2.0/DS/Sets/basic.py
@@ -0,0 +1,35 @@
+# in the python , the data - structure has set which is denoted as a symbol of {}
+
+# data = {} # it will be return the data type as dictionary <class 'dict'>
+
+# print(type(data))
+
+# ------------------------------------------------
+
+# data = set()
+
+# print(data) # set() will be return
+
+# print(type(data)) # <class 'set'>
+
+# -------------------------------------------------
+
+# data = set({1 , 4 , 7})
+
+# print(data) # it will be return the set as {1 , 4 , 7}
+
+# print(type(data)) # <class 'set'>
+
+# in the type casting of set if we write symbol as [] , () it will be always return as set{} -------------------------------------------------
+
+# data = set({1 , 4 , 7})
+
+# print(data) # it will be return the set as {1 , 4 , 7}
+
+# print(type(data)) # <class 'set'>
+
+data = set((1 , 4 , 7))
+
+print(data) # it will be return the set as {1 , 4 , 7}
+
+print(type(data)) # <class 'set'>
