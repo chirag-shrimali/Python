989ff870f3e935dc2a6c07e52c171925a79e6012
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-03-02 19:14:27 +0530

    Set Basics - 1

---
+++ Python_2.0/DS/Sets/basic1.py
@@ -0,0 +1,26 @@
+# it will be return the elements or members in the unordered way means no proper order it will be maintain and also duplication removed it will be always display elements only once a times...
+
+# data = {1 , 5 , 6 , 88 , 6 , 7 , 1 , 70}
+
+# print(data)
+
+# print(type(data))
+
+# we can not be access the elements or members by it's index value it's return error-----------------------------------------------
+
+# data = {1 , 5 , 6 , 88 , 6 , 7 , 1 , 70}
+
+# # print(data[3]) # error shows TypeError : 'set' object is not subscriptable
+
+# print(type(data))
+
+# we can only iterable for each loops -----------------------------------------------------------
+
+data = {10 , 50 , 6 , 88 , 6 , 7 , 10 , 70}
+
+print(data)
+
+print(type(data))
+
+for i in data :
+    print(i)
