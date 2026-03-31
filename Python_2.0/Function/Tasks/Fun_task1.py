4f242db027696bae4c0b65071ad3f7563ae8def2
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-03-31 16:54:35 +0530

    Function Task - 1

---
+++ Python_2.0/Function/Tasks/Fun_task1.py
@@ -0,0 +1,10 @@
+'''
+1. accept two string arguments and return full Name
+
+fname virat and lname kohli so the result will be print virat kohli
+'''
+
+def getFullName(fname , lname) :
+    return fname + ' ' + lname
+
+print(f"The Full_Name is : {getFullName('Chirag' , "Shrimali")}")
