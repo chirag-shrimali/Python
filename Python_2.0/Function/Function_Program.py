fd6bcad4a448a8e8231863213cdc4847b7a2a002
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-03-31 17:39:43 +0530

    Function Program

---
+++ Python_2.0/Function/Function_Program.py
@@ -0,0 +1,57 @@
+'''
+
+def avg(a , b , c) :
+
+    print("Average Function is Called...")
+
+    average = (a + b + c) / 3
+
+    print(f'The Average is : {average}')
+
+avg(1 , 2 , 3)
+
+'''
+
+# ------------------------------------------------------------------------
+
+"""
+
+def avg(a , b , c) :
+
+    print("Average Function is Called...")
+
+    return (a + b + c) / 3
+
+print(f'Average is : {avg(1 , 2 , 3)}')
+
+"""
+
+# ------------------------------------------------------------------------
+
+'''
+
+def avg(a , b , c) :
+
+    print("Average Function is Called...")
+
+    average = (a + b + c) / 3
+
+    return average
+
+print(f'Average is : {avg(1 , 2 , 3)}')
+
+'''
+
+# ------------------------------------------------------------------------
+
+def avg(a , b , c) :
+
+    print("Average Function is Called...")
+
+    average = (a + b + c) / 3
+
+    return average
+
+ans = avg(1 , 2 , 3)
+
+print(f'The Average is : {ans}')
