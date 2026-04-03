dcf5056de6e3d94c12234ef9da0102b9f037c5ee
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-04-03 20:24:38 +0530

    Numpy...

---
+++ NumPy/numpy1.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+a = np.array([6 , 2 , 2 , 7 , 5])
+
+print(a) # print the array of a like [1  2  3  4  5]
+
+print(type(a)) # <class 'numpy.ndarray'>
+
+print(a.ndim) # number of dimension --> 1
+
+print(a.shape) # number of (column , row) (5 , )
+
+print(a.max()) # 5
+
+print(a.min()) # 1
+
+print()
