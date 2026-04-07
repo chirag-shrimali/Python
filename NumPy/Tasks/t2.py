f93dd2915dcd7146f84894f3d7b306c17f091b8f
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-04-07 18:57:57 +0530

    Task - 2

---
+++ NumPy/Tasks/t2.py
@@ -0,0 +1,25 @@
+'''
+task :2 create  a array  using   np.arange (1,31)  ==> reshape (6,5) .
+
+    [[1,2,3,4,5],
+    [6,7,8,9,10],
+    [11,12,13,14,15],
+    [16,17,18,19,20],
+    [21,22,23,24,25],
+    [26,27,28,29,30]]
+
+print only  using  slicing : 
+
+[[11 12],
+[16 17]]
+'''
+
+import numpy as np
+
+a = np.arange(1 , 31).reshape(6 , 5)
+
+print(a)
+
+print('-----------------------------')
+
+print(a[2 : 4 , 0 : 2])
