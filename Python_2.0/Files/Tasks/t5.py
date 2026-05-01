06d3eaa68570cacffcf2fa633cfd06e6811b6787
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-05-01 19:03:31 +0530

    Task - 5

---
+++ Python_2.0/Files/Tasks/t5.py
@@ -0,0 +1,11 @@
+import shutil
+
+import os
+
+if os.path.exists("Python_2.0/Files/demo.txt") and os.path.exists("Python_2.0/File_Demo") :
+
+    print("Files are Found...")
+
+else :
+
+    print("Files Path are not Found...")
