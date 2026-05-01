7bcb72d49554ae1926bcca26b72c150bb01daea4
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-05-01 19:00:39 +0530

    Task - 6 mkdir and makedirs

--- Python_2.0/Files/Tasks/t6.py
+++ Python_2.0/Files/Tasks/t6.py
@@ -1,5 +1,11 @@
 import os
 
-ans = os.mkdir("Python_2.0\Hello")
+# ans = os.mkdir("Python_2.0\Hello")
+
+# print(ans)
+
+# -------------------------------------------------
+
+ans = os.makedirs("a/b/c/d")
 
 print(ans)
