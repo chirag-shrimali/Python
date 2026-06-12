a84eccddf4756a7e3e80434986377ed7c644b34d
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-06-12 19:31:53 +0530

    Operator Overloading - 1

---
+++ Python_2.0/Oops/Polymorphism/Operator_Overloading.py
@@ -0,0 +1,23 @@
+class Cart :
+
+    def __init__(self , items) :
+        
+        self.items = items
+
+    def __add__(self, other) :
+        
+        print("Self..." , self.items)
+
+        print("Other..." , other.items)
+
+        total = self.items["amount"] + other.items["amount"]
+
+        return total
+    
+c1 = Cart({"name" : "iphone" , "amount" : 12000})
+
+c2 = Cart({"name" : "samsung" , "amount" : 13000})
+
+final = c2 + c1
+
+print(final)
