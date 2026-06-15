9ada7ec326262fb99fd1bd654ec1daedf54a8818
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-06-15 19:10:09 +0530

    Task - 1

---
+++ Python_2.0/Exception_Handling/Tasks/t1.py
@@ -0,0 +1,17 @@
+# l1 = []
+
+# try :
+
+#     for i in range(1 , 5) :
+
+#         l1.append(i)
+
+#     print(l1)
+
+# except IndexError :
+
+#     print("\nIndex Out of the Range Error...")
+
+# except :
+
+#     print("\nError...")
